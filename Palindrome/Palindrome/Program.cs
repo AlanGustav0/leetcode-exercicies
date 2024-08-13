71551e3d8790711c59736a61789040870c1dcfd3
AlanGustav0 <alagustavo946@gmail.com>
2024-08-13 20:56:04 -0300

    Adicionar arquivos de projeto.

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,21 @@
+﻿if (IsPalindrome(1110111))
+{
+    Console.WriteLine("It is a Palíndrome number");
+}
+else
+{
+    Console.WriteLine("It is not a Palíndrome number");
+}
+
+bool IsPalindrome(int value)
+{
+    var chars = value.ToString().Reverse().ToList();
+
+    var comparableValue = "";
+
+    chars.ForEach(value => comparableValue += value.ToString());
+
+    if (value.ToString().Equals(comparableValue)) return true;
+
+    return false;
+}
