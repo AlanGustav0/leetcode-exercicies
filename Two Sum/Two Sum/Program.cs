f812fecb31b123c7bb6561d544cb1c6da4c23d75
AlanGustav0 <alagustavo946@gmail.com>
2024-08-17 14:57:43 -0300

    feature: Two Sum Exercice

---
+++ Two Sum/Two Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int[] numbers = [2, 5, 5, 11];
+
+var result = TwoSum(numbers, 10);
+
+foreach (var number in result)
+{
+    Console.WriteLine(number);
+}
+
+
+int[] TwoSum(int[] nums, int target)
+{
+    var outputIndex = new List<int>();
+
+    for(int i = 0;i < nums.Length -1; i++)
+    {
+        for(int j = i + 1;j < nums.Length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                outputIndex.Add(i);
+                outputIndex.Add(j);
+            }
+        }
+    }
+
+    return outputIndex.ToArray();
+}
